85ec45225f4da2e6529bef2484174cd7926da9d2
Uyiosa Enabulele <hiskonxeptz@gmail.com>
2016-11-13 01:17:31 +0100

    Implement gulp tasks for browser reload per file changes

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+"use strict"
+
+var gulp = require('gulp'),
+  browserSync = require('browser-sync'),
+  syncInstance1 = browserSync.create(),
+  syncInstance2 = browserSync.create();
+
+gulp.task('default', function() {
+  gulpSequence('reload-browser', 'reload-jasmine');
+});
+
+
+gulp.task('reload-jasmine', function() {
+  synInstance1.init({
+    server: {
+      baseDir: '.',
+      index: 'jasmine/specRunner.html'
+    },
+    port: 3000
+  })
+});
+
+
+
+gulp.task('reload-browser', function() {
+  syncInstance2.init({
+    server: {
+      baseDir: './public',
+      index: 'index.html'
+    }
+  })
+})
+
+
+
+gulp.watch('watch-changes', function() {
+  gulp.watch(['src/inverted-index.js']).on('change', syncInstance2.reload);
+})
