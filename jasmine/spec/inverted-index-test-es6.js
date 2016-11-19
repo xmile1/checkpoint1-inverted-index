447ba1edeb21a1eb35a6e2f905f4dc51d6a1d424
Uyiosa Enabulele <hiskoneptz@gmail.com>
2016-11-19 13:09:40 +0100

    chore(jasmine-test): refactor test suits with some reimplementation

--- jasmine/spec/inverted-index-test-es6.js
+++ jasmine/spec/inverted-index-test-es6.js
@@ -1,3 +1,4 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 const invalid = 'spec/dummy-data/empty.json ';
 const empty = 'spec/dummy-data/file0.json';
 const valid1 = 'spec/dummy-data/file1.json';
@@ -158,3 +159,5 @@ describe("Search Index", function() {
   });
 
 });
+
+},{}]},{},[1])
