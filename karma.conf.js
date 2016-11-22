a837bb522e69648c374d84d92c8c5394f0c55310
Uyiosa Enabulele <hiskoneptz@gmail.com>
2016-11-22 15:10:23 +0100

    bug(travis) change karma config to use chrome browser

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'src/inverted-index.js',
+      'jasmine/spec/inverted-index-es5.js',
       'jasmine/spec/inverted-index-test.js'
     ],
 
@@ -54,7 +54,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
+    browsers: ['Chrome'],
 
 
     // Continuous Integration mode
