69aa3518ed579306c1e55882e39577f986f748c6
Uyiosa Enabulele <hiskonxeptz@gmail.com>
2016-12-05 20:31:45 +0100

    chore(refactor): refactor test

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -6,37 +6,41 @@ const invalidContent = require('./dummy-data/file4.json');
 const nonString = require('./dummy-data/file3.json');
 var Index = require('../../src/inverted-index.js');
 
-function getFile(filename, cb) {
-  const theResult = [filename];
-  cb(theResult);
-}
 describe('Read Book data', function() {
   const index = new Index();
+
   describe('When I upload a JSON file', () => {
-    it('It should checks if its a valid JSON array', (done) => {
+    it('should checks if its a valid JSON array', (done) => {
       expect(index.saveUploads('invalidStructure.json', invalidStructure)).toBeFalsy();
       done();
     });
-    it('It should check if the file is empty', (done) => {
+    it('should check if the file is empty', (done) => {
       expect(index.saveUploads('empty.json', empty)).toBeFalsy();
       done();
     });
-    it('It should check if Its property values are strings', (done) => {
+    it('should check if Its property values are strings', (done) => {
       expect(index.saveUploads('nonString.json', nonString)).toBeFalsy();
       done();
     });
-    it('get json database should return the saved content', (done) => {
+  });
+
+  describe('When I call the getJsonDatabase function', () => {
+    it('should return the saved content', (done) => {
       index.saveUploads('valid1.json', valid1);
       expect(Object.keys(index.getJsonDatabase()).length).toEqual(1);
       done();
     });
-    it('getfilename should return the filenames of the saved contents', (done) => {
+  });
+
+  describe('When I call the getFileName function', () => {
+    it('should return the filenames of the saved contents', (done) => {
       index.saveUploads('valid1.json', valid1);
-      expect(index.getFileNames()).toEqual(['valid1.json']);
+      expect(Object.keys(index.getJsonDatabase()).length).toEqual(1);
       done();
     });
   });
 });
+
 describe('Populate Index', () => {
   const index = new Index();
   index.saveUploads('valid1.json', valid1);
@@ -44,19 +48,19 @@ describe('Populate Index', () => {
   index.createIndex('valid1.json');
 
   describe('When I Upload a File', () => {
-    it('the index is created once the JSON file has been read', (done) => {
+    it('should create an index', (done) => {
       expect(typeof index.getIndex('valid1.json')).toEqual('object');
       done();
     });
 
-    it('the created index should be an accurate one', (done) => {
+    it('should create an accurate index', (done) => {
       expect(index.getIndex('valid1.json').alice[0]).toEqual(0);
       expect(index.getIndex('valid1.json').lord[0]).toEqual(1);
       expect(index.getIndex('valid1.json').a[1]).toEqual(1);
       done();
     });
 
-    it('the created index should be an inverted index', (done) => {
+    it('should create an inverted index', (done) => {
       let verdict = true;
       const indexContent = index.indexFile['valid1.json'];
 
@@ -68,7 +72,7 @@ describe('Populate Index', () => {
       expect(verdict).toEqual(true);
       done();
     });
-    it('it should not overwrite the index', (done) => {
+    it('should not overwrite an already created index', (done) => {
       const indexBefore = index.getIndex('valid1.json');
       index.createIndex('valid2.json');
       const indexAfter = index.getIndex('valid1.json');
